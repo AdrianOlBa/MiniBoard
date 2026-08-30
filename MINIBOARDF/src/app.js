17275417c86d9c7a7371bc1a3e4d766a6bc8aafc
AdrianOLBa <pauloadrianoliva@gmail.com>
2026-08-30 13:44:35 +0200

    base_visual_Home

--- MINIBOARDF/src/app.js
+++ MINIBOARDF/src/app.js
@@ -2,9 +2,17 @@ import { BuildLogin } from "./controller/BuildLogin.js";
 
 //inicializar la aplicacion
 function app(){
-const AppRoot=document.getElementById('app');
+//const AppRoot=document.getElementById('app');
 const root=new BuildLogin(AppRoot);
 root.render(AppRoot);
 }
+
+let x = document.getElementById("Cards-Grid")
+
+new Sortable(x, {
+    animation: 150,
+    ghostClass: 'blue-background-class',
+    draggable: ".tarjeta",
+});
 
 app()
--- MINIBOARDF/src/view/Home_Page.js
+++ MINIBOARDF/src/view/Home_Page.js
@@ -1,10 +1,10 @@
-export function Home_Page(){
-const root = document.createElement("div");
-  root.className = 'row h-100 d-flex align-items-center justify-content-center';
-  root.id = "principalHome";
-  
+export function Home_Page() {
+    const root = document.createElement("div");
+    root.className = 'row h-100 d-flex align-items-center justify-content-center';
+    root.id = "principalHome";
+    root.innerHTML = ""
 
 
 
-    return{}
+    return {}
 }
